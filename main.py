91ed44ecce9a21d9f0d21cd3ff6c4dd64e66d860
bt <bogdan.tarus@gmail.com>
2022-04-11 23:07:38 +0200

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
 # import the WFDB package
 import wfdb
+
+record_header = wfdb.rdheader(record_name='f1o01', pn_dir='fantasia/1.0.0')
+signal_numbers = record_header.n_sig
+signal_names = record_header.sig_name
+
+print(type(record_header))
+print("signal_numbers: ", signal_numbers)
+print("signal_names: ", signal_names)
+
